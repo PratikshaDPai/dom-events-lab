7bc3afbe7ad66d2eef8255689ec604535cd99bcc
Pratiksha Pai <1312pratiksha@gmail.com>
2025-02-14 16:47:58 -0800

    Add clear function, conditionals for button values

--- app.js
+++ app.js
@@ -9,10 +9,16 @@ const displayElement = document.querySelector(".display");
 /*----------------------------- Event Listeners -----------------------------*/
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
+    const buttonVal = event.target.innerText;
     // This log is for testing purposes to verify we're getting the correct value
-    console.log(event.target.innerText);
-
-    displayElement.innerText += event.target.innerText;
+    console.log(buttonVal);
+    if (buttonVal === "C") {
+      clear();
+    } else if (buttonVal === "=") {
+      displayElement.innerText = eval(displayElement.innerText);
+    } else {
+      displayElement.innerText += event.target.innerText;
+    }
     // Future logic to capture the button's value would go here...
   });
 });
@@ -24,4 +30,6 @@ function subtract(num1, num2) {}
 function multiply(num1, num2) {}
 function divide(num1, num2) {}
 function displayOutput() {}
-function clear() {}
+function clear() {
+  displayElement.innerText = "0";
+}
