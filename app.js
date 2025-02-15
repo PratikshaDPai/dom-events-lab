52b1c888db7b02db3ae6e9cd11eea753e2549d12
Pratiksha Pai <1312pratiksha@gmail.com>
2025-02-14 16:37:06 -0800

    Add initial function names, display numbers on calculator screen

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 /*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll(".button");
+const displayElement = document.querySelector(".display");
 
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    // This log is for testing purposes to verify we're getting the correct value
+    console.log(event.target.innerText);
+
+    displayElement.innerText += event.target.innerText;
+    // Future logic to capture the button's value would go here...
+  });
+});
 
 /*-------------------------------- Functions --------------------------------*/
+
+function add(num1, num2) {}
+function subtract(num1, num2) {}
+function multiply(num1, num2) {}
+function divide(num1, num2) {}
+function displayOutput() {}
+function clear() {}
